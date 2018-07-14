7a0dbaabe7d5908d63b53f580093d8ade405143a
jiaxuanc <jiaxuan.chen@alumni.ubc.ca>
2018-07-13 22:34:59 -0700

    add componentDidMount() and use a web API to fetch robot users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css'
 
@@ -8,9 +7,15 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: '',
 		}
+	}
+
+	componentDidMount() {
+		fetch('http://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState( {robots: users} ));
 	}
 
 	onSearchChange = (event) => {
@@ -21,13 +26,17 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robot =>
 			robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 		)
-		return (
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots}/>
-			</div>
-		)
+		if (this.state.robots.length === 0)
+			return <h1>loading</h1>;
+		else {
+			return (
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<CardList robots={filteredRobots}/>
+				</div>
+			);
+		}
 	}
 }
 
