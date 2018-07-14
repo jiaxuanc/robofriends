7ad8adafe80ba8292fe3e9d39b2ee24aff0ce4df
jiaxuanc <jiaxuan.chen@alumni.ubc.ca>
2018-07-13 19:25:41 -0700

    create responsive robot cards to search given a list of robots

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			robots: robots,
+			searchfield: '',
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value});
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robot =>
+			robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		)
+		return (
+			<div className='tc'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<CardList robots={filteredRobots}/>
+			</div>
+		)
+	}
 }
 
 export default App;
